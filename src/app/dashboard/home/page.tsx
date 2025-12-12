35addce87659f4b75ad4e9421088afaa13465b56
Taddy <thaddadavis@gmail.com>
2025-12-11 21:21:07 -0500

    saving attempt to fix build error

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,14 +1,18 @@
 import { DashboardLayout } from "@/components/layouts/dashboard-layout";
-import React from "react";
+import React, { Suspense } from "react";
+
+export const dynamic = "force-dynamic";
 
 export default function Page() {
   return (
-    <DashboardLayout>
-      <>
-        <h1 className="text-center text-5xl leading-[1.5] font-semibold leading-12 text-ellipsis overflow-hidden text-text_default_color p-1">
-          Home
-        </h1>
-      </>
-    </DashboardLayout>
+    <Suspense fallback={<div>Loading...</div>}>
+      <DashboardLayout>
+        <>
+          <h1 className="text-center text-5xl leading-[1.5] font-semibold leading-12 text-ellipsis overflow-hidden text-text_default_color p-1">
+            Home
+          </h1>
+        </>
+      </DashboardLayout>
+    </Suspense>
   );
 }
